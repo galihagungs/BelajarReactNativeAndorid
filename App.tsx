40c4168e41bcf22546be0cc82be4e54e35b213f7
Galih Agung Sukmawan <galihagung223@gmail.com>
2023-05-29 21:49:41 +0700

    Belajar Component

--- App.tsx
+++ App.tsx
@@ -1,19 +1,26 @@
 import React from 'react';
 
-import {Text, View} from 'react-native';
+import {Image, Text, TextInput, View} from 'react-native';
 
 const App = () => {
   return (
     <View>
+      <View style={{ width: 80,height:80, backgroundColor:'#3498db' }}/>
       <Text>Galih</Text>
       <Text>Agung</Text>
       <Text>Sukmawan</Text>
-      {/* <Home /> */}
+      <Home />
+      <Foto/>
+      <TextInput style ={{ marginHorizontal:5 ,marginTop:10,borderWidth:1}}/>
     </View>
   );
 };
 
-// const Home = () => {
-//   return <Text>Home</Text>;
-// };
+const Home = () => {
+  return <Text>Home</Text>;
+};
+
+const Foto = ()=> {
+  return <Image source={{ uri:'https://placeimg.com/100/100/tech' }} style={{ width: 100,height:100 }}/>
+}
 export default App;
