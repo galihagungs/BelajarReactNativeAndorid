01fcb66a18044500e84f5ee16071334e4ee95004
Galih Agung Sukmawan <galihagung223@gmail.com>
2023-05-30 09:17:14 +0700

    Implement Styling

--- App.tsx
+++ App.tsx
@@ -2,13 +2,30 @@ import React, { Component } from 'react';
 
 import {Image, StyleSheet, Text, TextInput, View} from 'react-native';
 
+import ROG from './rog.jpg';
+
 const App = () => {
   return (
     <View>
-      <StylingReactNativeComponent />
+      <ImplementStylingComponent />
     </View>
   );
 };
+const ImplementStylingComponent = ()=>{
+  return (
+    <View style= {{ padding: 12, backgroundColor:'#f2f2f2',borderRadius:8, width:212,}}>
+      <Image source={ROG} style={{width: 188, height: 107,borderRadius:8}}/>
+      <Text style={{ fontSize:14, fontWeight:'bold', marginTop:16 }}>ROG Ally Z1 Extreme</Text>
+      <Text style={{ fontSize:12, fontWeight:'bold', marginTop:13, color: '#F2994A'}}>
+        Rp. 9.000.000
+      </Text>
+      <Text style={{ fontSize:12, fontWeight:'300', marginTop:12 }}>Jakarta Timur</Text>
+      <View style={{ backgroundColor:'#6FCF97', paddingVertical: 6, borderRadius:25, marginTop:20}}>
+        <Text style={{ fontSize:14, fontWeight:'600', color:'white', textAlign:'center'}}>BELI</Text>
+      </View>
+    </View>
+  );
+}
 
 const StylingReactNativeComponent = () =>{
   return (
